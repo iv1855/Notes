3bdc83fd1dcbad4b403aa6a053cba98d6a888e5b
K <rp901@yandex.ru>
2020-07-27 17:42:40 +0300

    aac5

--- app/src/main/java/com/example/notes/NotesDatabase.java
+++ app/src/main/java/com/example/notes/NotesDatabase.java
@@ -2,9 +2,12 @@ package com.example.notes;
 
 import android.content.Context;
 
+import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+
+@Database(entities = {Note.class}, version = 1, exportSchema = false)
 public abstract class NotesDatabase extends RoomDatabase {
 
     private static NotesDatabase database;
@@ -14,9 +17,13 @@ public abstract class NotesDatabase extends RoomDatabase {
     public static NotesDatabase getInstance(Context context) {
         synchronized (LOCK) {
             if (database == null) {
-                database = Room.databaseBuilder(context, NotesDatabase.class, DB_NAME).build();
+                database = Room.databaseBuilder(context, NotesDatabase.class, DB_NAME)
+                        .allowMainThreadQueries()
+                        .build();
             }
             return database;
         }
     }
+
+    public abstract NotesDao notesDao();
 }
